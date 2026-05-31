1b36d9f0235edce4d55e6a76c685a78fbf922b88
chandan <chandandigital6@gmail.com>
2026-05-31 23:07:33 +0530

    build

--- resources/views/layouts/app/header.blade.php
+++ resources/views/layouts/app/header.blade.php
@@ -2,23 +2,6 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
     <head>
         @include('partials.head')
-
-
-
-        <script>
-    (function () {
-        const appearance = localStorage.getItem('appearance') || 'system';
-
-        if (
-            appearance === 'dark' ||
-            (appearance === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches)
-        ) {
-            document.documentElement.classList.add('dark');
-        } else {
-            document.documentElement.classList.remove('dark');
-        }
-    })();
-</script>
     </head>
     <body class="min-h-screen bg-white dark:bg-zinc-800">
         <flux:header container class="border-b border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
@@ -104,7 +87,4 @@
 
         @fluxScripts
     </body>
-
-
-
 </html>
