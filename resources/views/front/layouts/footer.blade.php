e429bbef15611813e8ec0e6bf15e94348a3be45e
chandan <chandandigital6@gmail.com>
2026-07-09 23:39:28 +0530

    charte

--- resources/views/front/layouts/footer.blade.php
+++ resources/views/front/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <!-- Footer here -->
-<section class="octoberresultchart">
+{{-- <section class="octoberresultchart">
     <div class="container">
         <div class="row">
             <div class="col-md-12 text-center">
@@ -7,7 +7,7 @@
             </div>
         </div>
     </div>
-</section>
+</section> --}}
 
 {{-- <section class="tabel3">
     <div class="container-fluid">
